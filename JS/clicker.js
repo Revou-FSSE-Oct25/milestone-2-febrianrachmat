3a568eeeec9bab8bf22961e14dffde65f0630212
Rachmat Febrian <febrian.rachmat11@gmail.com>
2025-12-18 13:37:21 +0700

    fix : adding style and featurer to all games

--- JS/clicker.js
+++ JS/clicker.js
@@ -18,3 +18,31 @@ const timer = setInterval(() => {
     alert(`Game Over! Your score: ${score}`);
   }
 }, 1000);
+
+clickBtn.classList.add("shake");
+setTimeout(() => clickBtn.classList.remove("shake"), 200);
+
+const totalTime = 10;
+const timeProgress = document.getElementById("timeProgress");
+
+time--;
+timeDisplay.textContent = time;
+
+const percentage = (time / totalTime) * 100;
+timeProgress.style.width = percentage + "%";
+
+timeProgress.style.width = "0%";
+
+leaderboard.innerHTML = "";
+
+scores.forEach((item, index) => {
+  const li = document.createElement("li");
+  li.textContent = `${item.name} - ${item.score}`;
+
+  if (index === 0) li.classList.add("rank-1");
+  if (index === 1) li.classList.add("rank-2");
+  if (index === 2) li.classList.add("rank-3");
+
+  leaderboard.appendChild(li);
+});
+
